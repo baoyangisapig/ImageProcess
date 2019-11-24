808cda93cf309245f94c5528dd59afbe876a2d7c
yiming <yimingdeveloper@gmail.com>
2019-11-24 11:07:00 -0800

    fxxk sharpen

--- src/main/sharpenMain.java
+++ src/main/sharpenMain.java
@@ -2,19 +2,20 @@ package main;
 
 import java.io.IOException;
 
+import controller.BlurController;
 import controller.ImageController;
+import controller.SharpenController;
+import model.FileUtils;
+import model.Image;
 
 public class sharpenMain {
   public static void main(String[] args) throws IOException {
-    ImageController c1 = new ImageController();
-    double[][] filter1 = new double[][]{
-            {-1.0 / 8.0, -1.0 / 8.0, -1.0 / 8.0, -1.0 / 8.0, -1.0 / 8.0},
-            {-1.0 / 8.0, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0, -1.0 / 8.0},
-            {-1.0 / 8.0, 1.0 / 4.0, 1.0, 1.0 / 4.0, -1.0 / 8.0},
-            {-1.0 / 8, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0, -1.0 / 8.0},
-            {-1.0 / 8.0, -1.0 / 8.0, -1.0 / 8.0, -1.0 / 8.0, -1.0 / 8.0}
-    };
-    c1.sharpen(filter1, "./res/panda.jpg", "./res/panda_sharpen.jpg");
-    c1.sharpen(filter1, "./res/kid.jpg", "./res/kid_sharpen.jpg");
+    Image pandaImg = (Image) FileUtils.load("./res/panda.jpg");
+    Image kidImg = (Image) FileUtils.load("./res/kid.jpg");
+    SharpenController controller = new SharpenController();
+    Image bluredPanda = (Image) controller.sharpen(pandaImg);
+    Image bluredKid = (Image) controller.sharpen(kidImg);
+    FileUtils.save(bluredPanda, "./res/panda_sharpen2.jpg");
+    FileUtils.save(bluredKid, "./res/kid_sharpen2.jpg");
   }
 }
